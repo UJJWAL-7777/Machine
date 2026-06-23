dbb7fed0cfb053838891d42ea3f41d2e41a0eea8
UJJWAL 7777 <unarayan7777@gmail.com>
2026-06-23 12:12:12 +0530

    new expense push

---
+++ Machinecoding/expense/backend/routes/expenseRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.router();
+
+const Expense = require("../models/Expense");
+
+router.post("/", async(req, res) => {
+    try{
+        const expense = await Expense.create(req.body);
+        res.status(201).json(expense);
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+});
+
+router.get("/", async( req, res) => {
+    try{
+        const expense = await Expense.find().sort({createdAt: -1});
+        res.json(expenses);
+    }
+    catch(err){
+        res.statis(500).json({message: err.message});
+    }
+});
+
+router.delete("/:id", async(req, res) => {
+    try{
+        await Expense.findByIdAndDelete(req.params.id);
+        res.json({message: "Expense Deleted"});
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+});
+
+module.exports = router;
