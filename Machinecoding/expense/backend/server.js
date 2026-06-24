05d134d57d4c1ce6cab9190553f058b284a6aaf7
UJJWAL 7777 <unarayan7777@gmail.com>
2026-06-24 21:29:00 +0530

    todays server commit

--- Machinecoding/expense/backend/server.js
+++ Machinecoding/expense/backend/server.js
@@ -2,3 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api/expenses", require("./routes/expenseRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server Running On Port ${PORT}`);
+});
